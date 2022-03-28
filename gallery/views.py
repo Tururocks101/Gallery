c54d2b13f0ebc31530af24643be1512144192195
Tururocks101 <95218150+Tururocks101@users.noreply.github.com>
2022-03-28 16:53:43 +0300

    update

--- gallery/views.py
+++ gallery/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from .models import Images, Locations
 
 # Create your views here.
+def homepage(request):
+    return render(request, 'index.html')
+
+def gallery(request):
+    pictures = Images.get_all()
+    locations = Locations.get_all()
+    return render(request, 'gallery.html', {'pictures': pictures, 'locations':locations})
+
+def search(request):
+    if 'category' in request.GET and request.GET['category']:
+        search_term = request.GET.get('category')
+        res = Images.search_image(search_term)
+        message = f'{search_term}'
+
+        return render(request, 'search.html', {'message':message, 'results':res})
+    else:
+        message = 'You have not searched any term'
+        return render(request, 'search.html', {'message':message})
+
+def location(request,locale):
+    images = Images.filter_by_location(locale)
+    return render(request, 'location.html', {'results':images})
